d290e7782339009619a7c4f38e3978eeea4157b2
2019920393 <2019920393@qq.com>
2025-06-28 13:36:47 +0800

    docs:文档更新

--- 01-java-Hello-world/code-snippets/Welcome.java
+++ 01-java-Hello-world/code-snippets/Welcome.java
@@ -2,5 +2,8 @@
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to java !");
+        System.out.println("Programming is fun !");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
     }
 }
